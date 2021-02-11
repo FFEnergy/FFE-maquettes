8445dc9ec9ac764ab32384c599f45fd70d31df97
floretEden <fmartinez@edensoolfr>
2021-02-11 13:30:19 +0100

    page index (section 1)
    section 1 faite

---
+++ index/JS/script.js
@@ -0,0 +1,10 @@
+
+var images = $( ".nuage" );
+console.log($(".nuage"));
+var d1 = $(".nuage:nth-child(2)").css("left").split('p')[0];
+
+$(window).scroll(function(){
+  $(".nuage:nth-child(1)").css({"marginLeft": ($(window).scrollTop()) + "px"});
+  $(".nuage:nth-child(2)").css({"left": (d1 - $(window).scrollTop()) + "px"});
+  $(".nuage:nth-child(3)").css({"marginLeft": ($(window).scrollTop()) + "px"});
+});
