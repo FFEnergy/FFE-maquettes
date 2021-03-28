16fec5992231226bdd393a5544d38957469e8064
Florent Martinez <56638314+FlorentEden@users.noreply.github.com>
2021-03-28 11:17:48 +0200

    section Compte refaite + changement de style sur page index

--- page-test/JS/script.js
+++ page-test/JS/script.js
@@ -47,24 +47,10 @@ $(window).resize(function() {
   }
 })
 
-var d1 = $(".nuage:nth-child(2)").css("left").split('p')[0];
-$(window).scroll(function() {
-  $(".nuage:nth-child(1)").css({
-    "marginLeft": ($(window).scrollTop()) + "px"
-  });
-  $(".nuage:nth-child(2)").css({
-    "left": (d1 - $(window).scrollTop()) + "px"
-  });
-  $(".nuage:nth-child(3)").css({
-    "marginLeft": ($(window).scrollTop()) + "px"
-  });
-});
-console.clear();
-var controller = new ScrollMagic.Controller();
-var scene2 = new ScrollMagic.Scene({
-    triggerElement: "#trigerBird"
-  })
-  .setTween(".oiseau", 1.5, {
-    left: -250
-  })
-  .addTo(controller);
+$(".section").click(function() {
+  $(".section").removeClass('actif');
+  $(this).toggleClass('actif');
+  $(".main").removeClass('actifMain');
+  $("."+this.id).toggleClass('actifMain');
+
+})
