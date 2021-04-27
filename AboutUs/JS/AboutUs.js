557270312ab5358c5f042035e4f8cadb1468a8f0
FlorentEden <fmartinez@edenschool.fr>
2021-04-27 16:57:11 +0200

    change page about us

--- AboutUs/JS/AboutUs.js
+++ AboutUs/JS/AboutUs.js
@@ -6,3 +6,29 @@ $(window).scroll(function(){
   $(".nuage:nth-child(2)").css({"left": (d1 - $(window).scrollTop()) + "px"});
   $(".nuage:nth-child(3)").css({"marginLeft": ($(window).scrollTop()) + "px"});
 });
+
+AOS.init();
+
+$(function () { // wait for document ready
+  // init
+  var controller = new ScrollMagic.Controller({
+    globalSceneOptions: {
+      triggerHook: 'onLeave',
+      duration: "100%" // this works just fine with duration 0 as well
+      // However with large numbers (>20) of pinned sections display errors can occur so every section should be unpinned once it's covered by the next section.
+      // Normally 100% would work for this, but here 200% is used, as Panel 3 is shown for more than 100% of scrollheight due to the pause.
+    }
+  });
+
+  // get all slides
+  var slides = document.querySelectorAll("section.panel");
+
+  // create scene for every slide
+  for (var i=0; i<slides.length; i++) {
+    new ScrollMagic.Scene({
+        triggerElement: slides[i]
+      })
+      .setPin(slides[i], {pushFollowers: false})
+      .addTo(controller);
+  }
+});
