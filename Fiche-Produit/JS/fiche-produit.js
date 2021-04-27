3e677d675f269854b5fd52bd267d7169428322cf
Florent Martinez <59737229+MartiFun@users.noreply.github.com>
2021-04-27 22:24:40 +0200

    essai change style page fiche produit

--- Fiche-Produit/JS/fiche-produit.js
+++ Fiche-Produit/JS/fiche-produit.js
@@ -1,19 +1,25 @@
-var leinput = document.getElementById('nombreEolienne')
-var valeur = document.getElementById('nombreEolienne').value
+AOS.init();
 
-function AjouterValeur() {
-    valeur = document.getElementById('nombreEolienne').value
-    valeur++;
-    leinput.value = valeur;
-}
-
-function EnleverValeur() {
-    valeur = document.getElementById('nombreEolienne').value
-    if (valeur > 0) {
-        valeur--;
-    }else {
-        valeur = 0;
+$(function () { // wait for document ready
+  // init
+  var controller = new ScrollMagic.Controller({
+    globalSceneOptions: {
+      triggerHook: 'onLeave',
+      duration: "200%" // this works just fine with duration 0 as well
+      // However with large numbers (>20) of pinned sections display errors can occur so every section should be unpinned once it's covered by the next section.
+      // Normally 100% would work for this, but here 200% is used, as Panel 3 is shown for more than 100% of scrollheight due to the pause.
     }
-    
-    leinput.value = valeur;
-}
+  });
+
+  // get all slides
+  var slides = document.querySelectorAll("section.panel");
+
+  // create scene for every slide
+  for (var i=0; i<slides.length; i++) {
+    new ScrollMagic.Scene({
+        triggerElement: slides[i]
+      })
+      .setPin(slides[i], {pushFollowers: false})
+      .addTo(controller);
+  }
+});
